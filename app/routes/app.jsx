0b62434e46f0859de9a94ca56444edc09d0e2edd
ImmBa1 <immbishe@gmail.com>
2025-12-30 10:27:51 -0800

    Wrap embedded app with App Bridge provider

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,16 +1,23 @@
-import { Outlet, useRouteError } from "react-router";
+import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
+import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
   // eslint-disable-next-line no-undef
-  return null;
+  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
 };
 
 export default function App() {
-  return <Outlet />;
+  const { apiKey } = useLoaderData();
+
+  return (
+    <AppProvider embedded apiKey={apiKey}>
+      <Outlet />
+    </AppProvider>
+  );
 }
 
 // Shopify needs React Router to catch some thrown responses, so that their headers are included in the response.
