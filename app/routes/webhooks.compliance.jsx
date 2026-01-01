901295206188850696f65fb85fcb94501d86b825
ImmBa1 <damir.sarsenov12@gmail.com>
2026-01-01 11:29:27 -0800

    change webhooks

---
+++ app/routes/webhooks.compliance.jsx
@@ -0,0 +1,10 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  try {
+    await authenticate.webhook(request);
+  } catch (error) {
+    console.error("Webhook auth failed", error);
+  }
+  return new Response(null, { status: 200 });
+};
